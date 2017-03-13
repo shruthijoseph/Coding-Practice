e1c9ef5ff86f2d971b49a12e7186fe5e769bceec
Shruthi <shruthiagil@yahoo.in>
2017-03-14 01:40:14 +0530

    Created a JavaProject named ProjectCostCalculator Created Classname 'CalculatorProject' with 'public' type as modifier Created method stubs. Imported package javax.swing.JOptionPane to make dialogs

---
+++ ProjectCostCalculator/src/CostCalculator.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class CostCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
