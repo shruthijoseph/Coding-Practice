5a6a529cf182970ff7b9fad23bb980a2e4d32dfe
Shruthi <shruthiagil@yahoo.in>
2017-03-14 01:55:31 +0530

    Declared materialMarkup variable and initialized it the given values based on the material. Created a function "Calculatecost" to calculate the final price Declared CalculatedCostVariable to store the final result returned by the "Calculatecost" method call.

--- ProjectCostCalculator/src/CostCalculator.java
+++ ProjectCostCalculator/src/CostCalculator.java
@@ -1,6 +1,9 @@
 import javax.swing.JOptionPane;
 public class CostCalculator {
 
+	public static double calculateCost(double bp,int n,double mm)
+	 {
+	 }
 	/**
 	 * @param args
 	 */
@@ -10,6 +13,7 @@ public class CostCalculator {
 		double basePrice;
 		int no_of_persons;
 		String material;
+		
 		userStringInput=JOptionPane.showInputDialog("Enter the base price");
 				basePrice=Double.parseDouble(userStringInput);
 		userStringInput=JOptionPane.showInputDialog("Enter the number of people");
@@ -17,6 +21,15 @@ public class CostCalculator {
 		userStringInput=JOptionPane.showInputDialog("Enter the material");
 		        material=userStringInput;
 
+		        if (material.equals("food"))
+					materialMarkup=0.13;
+				if(material.equals("drugs"))
+					materialMarkup=0.075;
+				if(material.equals("electronics"))
+					materialMarkup=0.02;
+				
+				CalculatedCost=calculateCost(basePrice,no_of_persons,materialMarkup);
+				JOptionPane.showMessageDialog(null,String.format("The calculated Price is"+CalculatedCost));
 
 	}
 
