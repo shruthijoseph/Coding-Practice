26afc84b66ef467ffa937403287011327e29287d
Shruthi <shruthiagil@yahoo.in>
2017-03-14 01:46:50 +0530

    Declared variables(basePrice,no_of_persons,material) to hold the user inputs extracted in variable 'userStringInput'

--- ProjectCostCalculator/src/CostCalculator.java
+++ ProjectCostCalculator/src/CostCalculator.java
@@ -5,7 +5,18 @@ public class CostCalculator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String userStringInput;
+		double basePrice;
+		int no_of_persons;
+		String material;
+		userStringInput=JOptionPane.showInputDialog("Enter the base price");
+				basePrice=Double.parseDouble(userStringInput);
+		userStringInput=JOptionPane.showInputDialog("Enter the number of people");
+		        no_of_persons=Integer.parseInt(userStringInput);
+		userStringInput=JOptionPane.showInputDialog("Enter the material");
+		        material=userStringInput;
+
 
 	}
 
